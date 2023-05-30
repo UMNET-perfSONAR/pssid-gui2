4402c4dea412eb09b51de962a7d34f9cc62d1ae8
Sarah Stec <74212084+sastec17@users.noreply.github.com>
2023-05-30 13:30:13 -0400

    Delete server directory - Need to reformat repo

--- server/src/routes/schedules.routes.ts
+++
@@ -1,9 +0,0 @@
-import express, { Express, Request, Response } from 'express';
-import { connectToMongoDB } from '../services/ideas.service';
-var schedules = express.Router();
-
-const getSchedules = require('../controllers/schedules.controllers');
-
-schedules.get('/', getSchedules);
-
-module.exports=schedules;
