0bd2cb69e77872d90a234801e2059008df89532a
Sarah Stec <sastec@umich.edu>
2023-07-03 14:39:39 -0400

    added test cron functionality to test searchbar.vue file. unsure if i want to keep vuetify or stick with a bootstrap modification.

--- services/client/src/views/searchbar.vue
+++ services/client/src/views/searchbar.vue
@@ -1,66 +1,39 @@
 <template>
-    <div class="col-md-6">
-        <!-- regex search bar-->
-        <input style="margin-bottom: 1em"
-            type="text"
-            v-model="searchKey"
-            placeholder="Search..."
-        />
-        <ul class="list-group">
-            <li 
-            class="list-group-item"
-            :class="{active: index == currentIndex}"
-            v-for="(item, index) in filteredData"
-            :key="index"
-            @click="setActiveItem(item, index)"
-            >{{ item.name }}
-            </li>
-        </ul>
+    <div>
+  
+      <cron-vuetify
+        v-model="value"
+        :chip-props="{ color: 'success', textColor: 'white' }"
+        @error="error=$event" />
+  
+      <!-- editable cron expression -->
+      <v-text-field
+        class="pt-3"
+        :modelValue="value"
+        @update:model-value="nextValue = $event"
+        @blur="value = nextValue"
+        label="cron expression"
+        :error-messages="error" />
+  
     </div>
-</template>
-
-<script>
-import { defineComponent } from 'vue';
-import { useHostStore } from '/src/stores/host_store';
-    export default defineComponent ({
-        props: {
-            items: {
-                type: Array,
-                required: true
-            },
-        },
-        data() {
-            return {
-                searchKey: '',
-                filteredData: [],
-                currentItem: {},
-                currentIndex: {}
-            }
-        },
-        // what to watch for 
-
-        watch: {
-            searchKey() {
-                this.filterData();
-            },
-            items() {
-                this.filterData();
-            }
-
-        },
-        mounted() {
-            this.filterData();
-        },
-        methods: {
-            // TODO: Add regex data filtering algorithm here
-            filterData() {
-                const regex = new RegExp(this.searchKey, 'i');
-                this.filteredData = this.items.filter(item => regex.test(item.name))
-            },
-            setActiveItem(item, index=1) {
-                this.currentItem = item,
-                this.currentIndex = index
-            }
-        }
-    })
-</script>
+  </template>
+  
+  <script>
+  export default {
+  
+    props: {
+      init: {
+        type: String,
+        default: '* * * * *'
+      }
+    },
+  
+    data () {
+      return {
+        value: this.init,
+        nextValue: this.init,
+        error: ''
+      }
+    }
+  }
+  </script>
