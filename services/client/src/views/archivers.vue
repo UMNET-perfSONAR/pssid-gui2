c3758300393b2f28df252cd6a755ac00076b19d1
Sarah Stec <sastec@umich.edu>
2023-07-07 14:57:12 -0400

    added dynamic rendering of optional data to archivers. need to error check.

--- services/client/src/views/archivers.vue
+++ services/client/src/views/archivers.vue
@@ -59,7 +59,8 @@
             <!-- Dynamically render add form information -->
             <div v-if="showForm===true">
               <dynamicForm @formData="handleSubmit"
-              :form_data="archiverStore.selectedArchiver">
+              :form_layout="archiverStore.selectedArchiver"
+              :optional_data="optional_data">
               </dynamicForm>
             </div>
           </form>
@@ -95,9 +96,10 @@
           <!-- render edit component if type of archiver remains the same -->
           <div v-if="formType === archiver_type"> 
             <editFormComp :current_item="currentItem" 
+                :dynamic_options="curr_data"
                 @deleteItem="deleteArchiver"
-                @editItem="editArchiver">
-            </editFormComp>
+                @editItem="editArchiver"> 
+            </editFormComp> 
           </div>
           <div v-else> 
             <dynamicForm :form_data="this.archiverStore.selectedArchiver"
@@ -116,20 +118,27 @@ import { useArchiverStore } from '../stores/archiver.store.ts';
 import dynamicForm from '../components/dynamicform.vue';
 import editFormComp from '../components/edit_dynamic_form.vue';
 import VueMultiselect from 'vue-multiselect';
+import dynamic_add_data from '../components/dynamic_add_data.vue';
   export default {
-    components: { dynamicForm, VueMultiselect, editFormComp },
+    components: { dynamicForm, VueMultiselect, editFormComp, dynamic_add_data },
     data() {
       return {
         // variables cor add form
         selected_archiver: '',
         archiver_name:'',
+        optional_data: [{
+          "key": '',
+          "value": ''
+        }],
 
         // manage view of pages 
         display: 'add',
         showForm: false,
         currentIndex: {},
         currentItem: {},
+        Item: {},
         
+        curr_data: [],
         // rendering of dynamic edit form 
         formType: '',
         archiver_type: '',
@@ -144,15 +153,41 @@ import VueMultiselect from 'vue-multiselect';
       await this.archiverStore.getArchiverNames();
     },
     methods: {
-      setActiveArchiver(archiver, index=1) {
-        this.currentIndex=index;
-        this.currentItem=archiver;
-        this.old_archiver_name=archiver.name;
+      async setActiveArchiver(archiver, index=1) {
+        // TODO - CLEANUP THIS FUNCTION 
+
+        let ind=0;
+        const data = JSON.parse(JSON.stringify(archiver.data))
+        // load num 
         this.formType = archiver.archiver;
+        await this.archiverStore.getDesiredArchiver(this.formType, 'selected')
+        
+        // use to extract data from archivers.data
+        const myJson = '{}';
+        let json_object = JSON.parse(myJson);
+        let param_data = JSON.parse(myJson);
+        this.curr_data = []
+
+        for (const [key,value] of Object.entries(data)) {
+          if (ind++ < this.archiverStore.selectedArchiver.length-1) {
+             console.log((`${key}:${value}`));
+             json_object[key] = value;
+          }
+          else {
+            this.curr_data.push({'key':key, 'value':value})
+          }
+        };
+
+        this.currentIndex=index;
+        this.currentItem={
+          name: archiver.name,
+          data: json_object,
+          archiver: archiver.archiver
+        };
+        // set display related variables
+        this.old_archiver_name=archiver.name;
         this.archiver_type = archiver.archiver;
-
         this.display=''
-
       },
       addArchiverForm() {
         this.display='add';
@@ -165,12 +200,16 @@ import VueMultiselect from 'vue-multiselect';
                     result[item.name] = item.value
                     return result
                     }, {});
+        const appended_data=this.curr_data.reduce((result, item)=> {
+                              result[item.key] = item.value
+                              return result
+                            }, {})
 
         const object = {
           "old_arc_name" : this.old_archiver_name,
           "new_arc_name" : this.currentItem.name,
           "archiver" : this.currentItem.archiver,
-          "data" : data
+          "data" : Object.assign(data, appended_data)
         }
         await this.archiverStore.editArchiver(object);
         await this.archiverStore.getArchivers();
@@ -196,13 +235,28 @@ import VueMultiselect from 'vue-multiselect';
       // format and submit data to DB
       async handleSubmit(form_data) {
         const spec_object = form_data.reduce((result, item)=> {
-                    result[item.name] = item.value
-                    return result
+          console.log(item.name);
+          if (item.name==="Optional Data") {
+            return result;
+          }
+          result[item.name] = item.value
+          return result;        
         }, {});
+
+        console.log(spec_object);
+        const data_object = this.optional_data.reduce((result, item)=> {
+          result[item.key] = item.value
+          return result
+        }, {});
+
+        console.log(data_object)
+
+        const obj = Object.assign(spec_object, data_object)
+        console.log(obj)
         await this.archiverStore.addArchiver({
           name: this.archiver_name,
           archiver: this.selected_archiver,
-          data: spec_object
+          data: obj
         })
         this.archiver_name = '';
         this.archiver = '';
