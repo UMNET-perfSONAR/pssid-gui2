c9ef2e0a023afe3cb871197e4b4ebb6d4f043d34
thealvinjg <alvinjg@umich.edu>
2025-06-13 13:26:18 -0400

    v1.0.2: Fixed issue where tests selected for a given batch is not being displayed when editing that batch.

--- services/client/src/views/batches.vue
+++ services/client/src/views/batches.vue
@@ -171,9 +171,11 @@
      await this.batchStore.getBatches();
     //  console.log(this.batchStore.getBatches())
      await this.SsidStore.getSsidProfiles();
+    //  console.log(this.SsidStore.getSsidProfiles());
      await this.JobStore.getJobs();
     //  console.log(this.JobStore.getJobs());
      await this.TestStore.getTests(); // this is how you retrieve tests that were made
+    //  console.log(this.TestStore.getTests())
      await this.scheduleStore.getSchedules();
      // hardcode layout of batches form - edit this to add more fields
      this.form_layout = [
@@ -228,6 +230,8 @@
        this.currentIndex=indexArray[1];
        this.showAddBatch = false;
        this.old_batchname=this.currentItem.name;
+       // currentItem.jobs stores jobs as an array, even though there is only one job per batch. This could be turned into a string
+       this.currentItem.tests = this.JobStore.jobs.filter((job) => job.name == this.currentItem.jobs[0])[0].tests;
      },
      
      /**
@@ -237,7 +241,7 @@
      async addBatch(form_data) {
        // create a hidden job to be added to the batch, job name is based off of current batch
        const job = {
-        name: `${form_data[0].value}_job`,
+        name: `job_${form_data[0].value}`,
         tests: (form_data[3].selected.length == 0)? [] : form_data[3].selected.map(obj => obj.name), // use selected tests from the form
         "continue-if": "true",  // default continue-if value
         backoff: "PT1S",  // default backoff value
@@ -254,24 +258,55 @@
          jobs: [job.name],
          tests: (form_data[3].selected.length == 0)? [] : form_data[3].selected.map(obj => obj.name),
        });
+      //  console.log((form_data[3].selected.length == 0)? [] : form_data[3].selected.map(obj => obj.name));
        this.addBatchForm();
      },
 
      // Edits a selected batch
      async editBatch() {
+        // deletes old job and adds a new one
+        const oldJobName = `job_${this.old_batchname}`;
+        const newJobName = `job_${this.currentItem.name}`;
+
+        try {
+          await this.JobStore.deleteJob({name: oldJobName});
+        } catch (error) {
+          console.error('Error deleting old job:', error);
+        }
+        
+        console.log(this.currentItem.tests);
+        console.log("-");
+        const updatedJob = {
+          name: newJobName,
+          // if there exists currentItem.tests and length is greater than 0, use it, otherwise empty array
+          tests: (this.currentItem.tests && this.currentItem.tests.length > 0) ? this.currentItem.tests : [],
+          "continue-if": "true",
+          backoff: "PT1S"
+        }
+
+        try {
+          await this.JobStore.addJob(updatedJob);
+        } catch (error) {
+          console.error('Error adding (edited) job:', error)
+        }
+
        const updated_batch = {
          "old_batchname":this.old_batchname,
          "new_batchname":this.currentItem.name,
          "priority": this.currentItem.priority,
          "ssid_profiles": this.currentItem.ssid_profiles,
          "schedules": this.currentItem.schedules,
-        //  "jobs": this.currentItem.jobs,
+         "jobs": [newJobName],
          "tests": this.currentItem.tests,
          "test_interface": this.currentItem.test_interface
        };
        await this.batchStore.editBatch(updated_batch);
+       // also refresh JobStore to display updated tests in that job (part of hidden jobs)
+       await this.JobStore.getJobs();
        await this.batchStore.getBatches();
 
+       // TODO: find a better way to make tests for a batch persist after updating a batch
+       const newTests = this.currentItem.tests
        this.currentItem = this.batchStore.batches[this.currentIndex];
        this.updateActiveBatch([this.currentItem, this.currentIndex]);
      },
@@ -282,6 +317,7 @@
        if (this.currentItem.jobs) {
          try {
           await this.JobStore.deleteJob({ name: this.currentItem.jobs });
+          await this.JobStore.getJobs(); // get refreshed job (part of hidden jobs)
          } catch (error) {
           console.error('Error deleting job:', error);
          }
